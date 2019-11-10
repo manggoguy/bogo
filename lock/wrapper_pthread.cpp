691fc88301ea6fa5f963fadccb594a5b62cec11d
manggoguy <manggoguy@naver.com>
2019-11-11 04:39:03 +0900

    delete header file

--- lock/wrapper_pthread.cpp
+++ lock/wrapper_pthread.cpp
@@ -1,11 +1,6 @@
 #include <pthread.h>
 
-pthread_mutex_t mutex_lock;
-
-
-extern "C" void wrapper_mutex_init(){
-    pthread_mutex_init(&mutex_lock, NULL);
-}
+pthread_mutex_t mutex_lock=PTHREAD_MUTEX_INITIALIZER;
 
 extern "C" void wrapper_mutex_lock(){
     pthread_mutex_lock(&mutex_lock);
